ec150e2c54a07cb65c2a91744a715dd282b1144c
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2024-03-04 22:23:56 +0100

    displaying all objects

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+<script setup lang="ts">
+import Objects from './components/Objects.vue';
+</script>
+<template>
+  <h1>RPS 101</h1>
+  <Suspense>
+    <template #default>
+      <Objects />
+    </template>
+    <template #fallback>
+      <p>Loading...</p>
+    </template>
+  </Suspense>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Objects.vue
@@ -0,0 +1,46 @@
+
+<script setup lang="ts">
+import { defineComponent, ref, Suspense } from 'vue';
+import axios, { all } from 'axios';
+
+const allObjects = ref([]);
+
+await axios.get('https://rps101.pythonanywhere.com/api/v1/objects/all')
+    .then(response => {
+        allObjects.value = response.data;
+    })
+    .catch(error => {
+        console.log(error);
+    });
+</script>
+<template>
+    <div class="objects">
+        <div class="object" v-for="object in allObjects">
+            <p >{{ object }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.objects {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    color: black;
+}
+.object {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 30px;
+    width: 100px;
+    border: 1px solid black;
+    margin: 10px;
+    border-radius: 5px;
+    background-color: #f0f0f0;
+    color: black;
+}
+</style>
