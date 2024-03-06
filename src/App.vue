2833b2fddfad7cd1b1d300bf96f6c52e543ed275
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2024-03-06 15:05:33 +0100

    local storage

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,72 @@
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
-import Objects from './components/Objects.vue';
-import axios from 'axios';
 
-const ObjectsInfo = ref({
-  choosenObject: '',
-  allObjects: [],
-  randomObjects: []
+import { ref, watch, isProxy, toRaw, Suspense } from 'vue';
+import axios from 'axios';
+import Gestures from './components/Gestures.vue';
+
+const gesturesInfo = ref({
+  aG: [], // all Gestures
+  rG: [], // random Gestures
+  cG: '', // choosen Gesture
 });
 const outcome = ref<{ winner?: string, outcome?: string, loser?: string }>({});
 
-async function matchObjects(obj1 : string, obj2 : string) {
+async function matchObjects(gesture1 : string, gesture2 : string) {
   let apiAdress = 'https://rps101.pythonanywhere.com/api/v1/match?';
-  apiAdress += 'object_one=' + obj1 + '&object_two=' + obj2;
+  apiAdress += 'object_one=' + gesture1 + '&object_two=' + gesture2;
   
   await axios.get(apiAdress)
   .then(response => {
     outcome.value = response.data;
+    console.log(outcome.value)
   })
   .catch(error => {
     console.log(error);
   });
 }
 
-watch(ObjectsInfo, (newValue) => {
-  let allObjects = [];
-  if(newValue.randomObjects instanceof Array) allObjects = newValue.randomObjects;
-  else return;
-  const randomObject = allObjects[Math.floor(Math.random() * allObjects.length)];
+watch(gesturesInfo, (newValue) => {
+  var gesturesInfoRaw;
+  if(isProxy(newValue)){
+    gesturesInfoRaw = toRaw(newValue);
+  } else {
+    gesturesInfoRaw = newValue;
+  }
 
-  let apiAdress = 'https://rps101.pythonanywhere.com/api/v1/match?';
-  apiAdress += 'object_one=' + newValue.choosenObject + '&object_two=' + randomObject;
-
-  matchObjects(newValue.choosenObject, randomObject);
+  let avaliableGestures = gesturesInfoRaw.rG;
+  let randomGesture = avaliableGestures[Math.floor(Math.random() * avaliableGestures.length)];
+  
+  matchObjects(newValue.cG, randomGesture);
 });
 
-function checkIfOutcomeIsDefined() {
+function isOutcomeDefined(){
   return Object.keys(outcome.value).length > 0;
 }
 
-function checkWhoWon() {
+function whoWon(){
   if(outcome.value.winner == 'None - Draw') return 'It\'s a draw!';
-  else if(outcome.value.winner == ObjectsInfo.value.choosenObject) return 'You won!';
+  else if(outcome.value.winner == gesturesInfo.value.cG) return 'You won!';
   else return 'You lost!';
 }
+
 </script>
 <template>
   <h1>RPS 101</h1>
-  <div v-if="checkIfOutcomeIsDefined()">
-    <h2 v-if="checkWhoWon() == 'You won!'" class="won">{{ checkWhoWon() }}</h2>
-    <h2 v-else-if="checkWhoWon() == 'You lost!'" class="lost">{{ checkWhoWon() }}</h2>
-    <h2 v-else class="draw">{{ checkWhoWon() }}</h2>
-    <h3 v-if="checkWhoWon() != 'It\'s a draw!'">{{ outcome.winner }} {{ outcome.outcome }} {{ outcome.loser }}</h3>
-    <h3 v-else="checkWhoWon() != 'It\'s a draw!'">{{ ObjectsInfo.choosenObject }} {{ outcome.outcome }} {{ ObjectsInfo.choosenObject }}</h3>
+  <div v-if="isOutcomeDefined()">
+    <h2 v-if="whoWon() == 'You won!'" class="won">{{ whoWon() }}</h2>
+    <h2 v-else-if="whoWon() == 'You lost!'" class="lost">{{ whoWon() }}</h2>
+    <h2 v-else class="draw">{{ whoWon() }}</h2>
+
+    <h3 v-if="whoWon() != 'It\'s a draw!'">{{ outcome.winner }} {{ outcome.outcome }} {{ outcome.loser }}</h3>
+    <h3 v-else>{{ gesturesInfo.cG }} {{ outcome.outcome }} {{ gesturesInfo.cG }}</h3>
     <div class="button-wrap">
-      <button class="button" @click="outcome = {}">Play Again</button>
+      <button @click="outcome = {}">Play Again</button>
     </div>
   </div>
-  
   <Suspense v-else>
     <template #default>
-      <Objects v-model="ObjectsInfo"/>
+      <Gestures  v-model="gesturesInfo"></Gestures>
     </template>
     <template #fallback>
       <p>Loading...</p>
@@ -78,24 +83,5 @@ function checkWhoWon() {
 }
 .draw {
   color: #ffff55;
-}
-.button {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 50px;
-  width: 100px;
-  border: 1px solid black;
-  margin: 10px;
-  border-radius: 5px;
-  background-color: #f0f0f0;
-  color: black;
-}
-.button-wrap {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  width: 100%;
 }
 </style>
---
+++ src/components/Gestures.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import axios from 'axios';
+
+var allGestures : Array<string> = [];
+var randomGestures: Array<string> = [];
+var choosenGesture: string = '';
+
+const gesturesInfo = ref(
+    {
+        aG: allGestures,
+        rG: randomGestures,
+        cG: choosenGesture
+    }
+);
+
+const refresh = ref(0);
+
+if(localStorage.getItem('allGestures') === null) {
+    await axios.get('https://rps101.pythonanywhere.com/api/v1/objects/all')
+    .then(response => {
+        localStorage.setItem('allGestures', JSON.stringify(response.data));
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+allGestures = JSON.parse(localStorage.getItem('allGestures') as string);
+
+function getRandomGestures(numOfGestures : number) {
+    let randomGestures = [];
+    for(let i = 0; i < numOfGestures; i++) {
+        randomGestures.push(allGestures[Math.floor(Math.random() * allGestures.length)]);
+    }
+    return randomGestures;
+}
+
+randomGestures = getRandomGestures(3);
+
+const emit = defineEmits();
+
+watch(() => [choosenGesture, refresh.value], (newValue) => {
+    if (newValue !== null) {
+        gesturesInfo.value.aG = allGestures;
+        gesturesInfo.value.rG = randomGestures;
+        gesturesInfo.value.cG = String(newValue[0]);
+    }
+});
+watch(() => [gesturesInfo.value, refresh.value], (newValue) => {
+    emit('update:modelValue', newValue[0]);
+},{ deep: true});
+
+function setGesture(gesture : string){
+    refresh.value++;
+    choosenGesture = gesture;
+}
+</script>
+<template>
+    <div>
+        <h2>Choose a gesture</h2>
+        <div class="gestures button-wrap">
+            <button class="gesture" v-for="gesture in randomGestures" @click="setGesture(gesture)">
+                <p>{{ gesture }}</p>
+            </button>
+        </div>
+    </div>
+</template>
